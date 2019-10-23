b0b8985d0ca03c5607a32997f2e4f51ddb7a02f9
Neil Campbell <neildcampbell@hotmail.com>
2019-10-23 13:24:39 +0100

    added es6 class structure

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -1,41 +1,38 @@
 describe('TECH BANK', function(){
+    let myAccount;
+    beforeEach(()=> {
+        myAccount = new BankAccount()
+    })
     it('describes creation of bank account with 0 funds', function(){
-        let myAccount = new bankAccount()
         expect(myAccount.total).toEqual(0)
     })
     it('describes make a deposit and adds to debit funds', function(){
-        let myAccount = new bankAccount()
         myAccount.deposit(1000)
         var result = myAccount.total
         expect(result).toEqual(1000)
     })
     it('describes making a withdrawal and new debit is calculated', function(){
-        let myAccount = new bankAccount()
         myAccount.withdrawal(400)
         myAccount.deposit(1000)
         var result = myAccount.total
         expect(result).toEqual(600)
     })
     it('describes checking for date', function(){
-        let myAccount = new bankAccount()
         let result = myAccount.findDate()
-        expect(result).toEqual('21/10/2019')
+        expect(result).toEqual('23/10/2019')
     })
     it('describes printing out a statement', function(){
-        let myAccount = new bankAccount()
         let result = myAccount.print()
         expect(result).toEqual('date || credit || debit || balance\n')
     })
    it('describes doing a withdrawal and adding the line to the statement', function(){
-    let myAccount = new bankAccount()
     myAccount.withdrawal(500)
     let result = myAccount.print()
-    expect(result).toEqual('date || credit || debit || balance\n21/10/2019 ||  || 500 || -500.00\n')
+    expect(result).toEqual('date || credit || debit || balance\n23/10/2019 ||  || 500 || -500.00\n')
    })
    it('describes doing a deposit and adding the line to the statement', function(){
-    let myAccount = new bankAccount()
     myAccount.deposit(1500)
     let result = myAccount.print()
-    expect(result).toEqual('date || credit || debit || balance\n21/10/2019 || 1500 || || 1500.00\n')
+    expect(result).toEqual('date || credit || debit || balance\n23/10/2019 || 1500 || || 1500.00\n')
    })
 })
--- src/bank.js
+++ src/bank.js
@@ -1,35 +1,38 @@
-function bankAccount(){
-    this.total = 0
-    this.amount = 0
-    this.statement =[]
-}
-bankAccount.prototype.findDate = function(){
-    let today = new Date()
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth()+ 1 ).padStart(2, '0')
-    let year = today.getFullYear();
-    today =  dd + '/' + mm + '/' + year 
-    return today
-}
+class BankAccount {
+    constructor() {
+        this.total = 0
+        this.amount = 0
+        this.statement = []
+    }
 
-bankAccount.prototype.deposit = function(amount){
-    let date = this.findDate()
-    this.total+= amount
-    this.statement.unshift(date + ' || ' + amount + ' || || ' + this.total+ '.00\n')
-    return this.total
-}
+    findDate() {
+        let today = new Date()
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0')
+        let year = today.getFullYear();
+        today = dd + '/' + mm + '/' + year
+        return today
+    }
 
-bankAccount.prototype.withdrawal =  function(amount){
-    let date = this.findDate()
-    this.total -= amount
-    let line = date + ' ||  || '+ amount + ' || ' + this.total + '.00\n'
-    this.statement.unshift(line)
-    return this.total
-}
+    deposit(amount) {
+        let date = this.findDate()
+        this.total += amount
+        this.statement.unshift(date + ' || ' + amount + ' || || ' + this.total + '.00\n')
+        return this.total
+    }
 
-bankAccount.prototype.print = function(){
-    let header = 'date || credit || debit || balance\n'
-    this.statement.unshift(header)
-    let finalStatement = this.statement.join('')
-    return finalStatement
+    withdrawal = function (amount) {
+        let date = this.findDate()
+        this.total -= amount
+        let line = date + ' ||  || ' + amount + ' || ' + this.total + '.00\n'
+        this.statement.unshift(line)
+        return this.total
+    }
+
+    print = function () {
+        let header = 'date || credit || debit || balance\n'
+        this.statement.unshift(header)
+        let finalStatement = this.statement.join('')
+        return finalStatement
+    }
 }
