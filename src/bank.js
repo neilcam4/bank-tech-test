fc47340c0316e38805a2d788008b8c8ac8a1fb0e
Neil Campbell <neildcampbell@hotmail.com>
2019-10-24 05:57:45 +0100

    updated readme.md

--- src/bank.js
+++ src/bank.js
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-function bankAccount(){
-    this.total = 0.00
-    this.amount = 0.00
-    this.statement = []
-}
-bankAccount.prototype.findDate = function(){
-    let today = new Date()
-    let dd = String(today.getDate()).padStart(2, '0');
-    let mm = String(today.getMonth()+ 1 ).padStart(2, '0')
-    let year = today.getFullYear();
-    today =  dd + '/' + mm + '/' + year 
-    return today
-}
-
-bankAccount.prototype.deposit = function(amount){
-    let date = this.findDate()
-    let newAmount = parseFloat(amount).toFixed(2)
-    this.total += amount
-    let newTotal = parseFloat(this.total).toFixed(2)
-    this.statement.unshift(date + ' || ' + newAmount + ' || || ' + newTotal + '\n')
-    return newTotal
-}
-
-bankAccount.prototype.withdrawal =  function(amount){
-    let date = this.findDate()
-    this.total -= amount
-    let newAmount = parseFloat(amount).toFixed(2)
-    let newTotal = parseFloat(this.total).toFixed(2)
-    let line = date + ' ||  || '+ newAmount + ' || ' + newTotal + '\n'
-    this.statement.unshift(line)
-    return newTotal
-}
-=======
 class BankAccount {
     constructor() {
         this.total = 0
@@ -54,7 +20,6 @@ class BankAccount {
         this.statement.unshift(date + ' || ' + amount + ' || || ' + this.total + '.00\n')
         return this.total
     }
->>>>>>> 75caca1ef23cc29033bd3695f6ae866d369a9da7
 
     withdrawal = function (amount) {
         let date = this.findDate()
@@ -68,6 +33,6 @@ class BankAccount {
         let header = 'date || credit || debit || balance\n'
         this.statement.unshift(header)
         let finalStatement = this.statement.join('')
-        return finalStatement
+        return finalStatement 
     }
 }
